60d3e82810fa4a3a2fa7e66453db36d61749c5c3
san-pblr-gct <san.pblr.gct@gmail.com>
2017-01-10 13:20:35 +0530

    updated port to 80

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 app.use("/styles", express.static(__dirname + '/public/'));
 
 var feedback = require('./app.js')(app);
-var server = app.listen(3000, function(){
+var server = app.listen(80, function(){
   mongoose.connect('mongodb://santhosh:3FqOFuo8kXXcrIRp6Lb8l6icwHLm8QdKhENkRIJkWWWw2Q3tyU1ARKb5KdgUtGKVPaOhOFOF7ukgPNOz7Z4aXg==@santhosh.documents.azure.com:10250/?ssl=true');
 
 });
